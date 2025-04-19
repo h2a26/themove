30c3f72df0cb7f0c29ecf82ba0877c30945deea6
heinhtetaung <hha.heinms@gmail.com>
2025-04-19 11:30:01 +0630

    updated: metadata

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
       react(),
       tailwindcss(),
+      viteCompression()
   ],
 
 })
